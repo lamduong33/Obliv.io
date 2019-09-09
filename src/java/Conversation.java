6f0ce92fa12ded2190b51aab030de5132f095ea6
LamTDuong <lamthachduong@gmail.com>
2019-09-08 20:02:20 -0400

    Initial commit for Conversation and Message class

---
+++ src/java/Conversation.java
@@ -0,0 +1,10 @@
+/**
+ * A class that handles user conversations.
+ * This class should act as a container of messages
+ * and associated users. 
+ * @author Lam Duong
+ */
+public class Conversation
+{
+	
+}
